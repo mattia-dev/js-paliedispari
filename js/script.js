c05c0480d3022b05c3cf7d424974dd125f2b7713
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-09 14:59:58 +0200

    ex. finished

--- js/script.js
+++ js/script.js
@@ -5,27 +5,50 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
 // Infine, dichiariamo chi ha vinto.
 
+function getRandomNumber(min, max) {
+    var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNumber;
+}
+
+function addNumbers(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+
+function oddOrEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 var output = document.getElementById('output');
 
-var oddOrEven = prompt("Scegli pari o dispari.");
+var userChoice = prompt("Scegli pari o dispari.");
+
+var userChoiceLowerCase = userChoice.toLowerCase();
 
 var userNumber = parseInt(prompt("Scegli un numero da 1 a 5."));
 
-function getRandomNumber (min, max) {
-    var computerNumber = Math.floor(Math.random * (max - min + 1)) + 1;
-    return computerNumber;
+var resultNumber = getRandomNumber(1, 5);
+
+output.innerHTML = "Hai scelto " + userChoiceLowerCase + " e hai lanciato " + userNumber + ". Il computer lancia " + resultNumber + ".";
+
+var total = addNumbers(resultNumber, userNumber);
+
+var gameResult = "";
+
+if (oddOrEven(total)) {
+    gameResult = "pari";
+} else {
+    gameResult = "dispari";
 }
 
-var sum = userNumber + getRandomNumber(1, 5);
+output.innerHTML += "</br>Il risultato è " + gameResult + ".";
 
-output.innerHTML = sum;
-
-// function getNumberStatus (num) {
-//     var numberStatus = false;
-
-//     if (num % 2 === 0) {
-//         numberStatus = true;
-//     }
-
-//     return numberStatus;
-// }
+if (userChoiceLowerCase == gameResult) {
+    output.innerHTML += "</br>Hai vinto!!"
+} else {
+    output.innerHTML += "</br>Hai perso!";
+}
